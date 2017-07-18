f0cb4f67886586c0bb975a0861fabff28c44cee7
Dennis Köhl <dennis.koehl@schalk-and-friends.de>
2017-07-18 09:08:11 +0200

    Links and filesnames removed from code

--- index.php
+++ index.php
@@ -2,31 +2,34 @@
 
 getPageData();
 
-function getPageData(){
-  $fileContent = '';
-  $resultFileContent = file_get_contents('result.txt');
+function getPageData()
+{
+    $resultFile                 = 'result.txt';
+    $baseURL                    = 'http://ebook-hell.to';
+    $baseURLwithCategory        = 'http://ebook-hell.to/?cat=53&start=';
 
-  $oldLinkFileCounter = explode(PHP_EOL, trim($resultFileContent));
-  $newLinkCounter = 0;
-  $oldLinkCounter = 0;
+    $newLinkCounter             = 0;
+    $oldLinkCounter             = 0;
 
-  for($i=1; $i<=10; $i++){
-    $doc = new DOMDocument();
-    @$doc->loadHTMLFile('http://ebook-hell.to/?cat=53&start=' . $i);
-    foreach ($doc->getElementsByTagName('a') as $tag) {
-      if($tag->nodeValue == 'Download'){
-        if(strstr($resultFileContent, 'http://ebook-hell.to' . $tag->getAttribute('href'). PHP_EOL)){
-          $oldLinkCounter++;
-          continue;
+    $resultFileContent = file_get_contents($resultFile);
+    $oldLinkFileCounter = explode(PHP_EOL, trim($resultFileContent));
+    for ($i = 1; $i <= 10; $i++) {
+        $doc = new DOMDocument();
+        @$doc->loadHTMLFile($baseURLwithCategory . $i);
+        foreach ($doc->getElementsByTagName('a') as $tag) {
+            if ($tag->nodeValue == 'Download') {
+                if (strstr($resultFileContent, $baseURL . $tag->getAttribute('href') . PHP_EOL)) {
+                    $oldLinkCounter++;
+                    continue;
+                }
+                $newLinkCounter++;
+                file_put_contents($resultFile, $baseURL . $tag->getAttribute('href') . PHP_EOL, FILE_APPEND);
+            }
         }
-        $newLinkCounter++;
-        file_put_contents('result.txt', 'http://ebook-hell.to' . $tag->getAttribute('href'). PHP_EOL, FILE_APPEND);
-      }
     }
-  }
-  $incomingLinkCounts = count($oldLinkFileCounter);
-  echo ':::: Result ::::' . PHP_EOL;
-  echo ':: Incoming: ' . $incomingLinkCounts . PHP_EOL;
-  echo ':: New-Links: ' . $newLinkCounter . PHP_EOL;
-  echo ':: All Links: ' . ($incomingLinkCounts+$newLinkCounter) . PHP_EOL;
+    $incomingLinkCounts = count($oldLinkFileCounter);
+    echo ':::: Result ::::' . PHP_EOL;
+    echo ':: Incoming: ' . $incomingLinkCounts . PHP_EOL;
+    echo ':: New-Links: ' . $newLinkCounter . PHP_EOL;
+    echo ':: All Links: ' . ($incomingLinkCounts + $newLinkCounter) . PHP_EOL;
 }
